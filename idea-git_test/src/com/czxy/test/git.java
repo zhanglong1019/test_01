97eafb6f1cf1a47d47cd3227f412aeae327d1770
zhangheiyouhei <2498358940@.com>
2019-07-29 08:41:39 +0800

    添加test工程至git

---
+++ idea-git_test/src/com/czxy/test/git.java
@@ -0,0 +1,10 @@
+package com.czxy.test;
+
+public class git {
+        public static void main(String[] args) {
+            System.out.println("hello git1");
+            System.out.println("hello git2");
+            System.out.println("hello git3");
+        }
+
+}
